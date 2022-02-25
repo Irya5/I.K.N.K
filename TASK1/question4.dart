90e39e277762a82f191f8e3ec475bb369df6e408
Irya5 <100368720+Irya5@users.noreply.github.com>
2022-02-25 20:45:38 +0530

    Update question4.dart

--- TASK1/question4.dart
+++ TASK1/question4.dart
@@ -1,9 +1,6 @@
 import 'dart:io';
 
-double sum(double a, int b) {
-  print("sum is ");
-  return a + b;
-}
+
 
 double diff(double a, int b) {
   print("difference is ");
@@ -25,9 +22,29 @@ int main() {
       " enter numbers between whoem arthemetic operation has to be performes");
   double? a = double.parse(stdin.readLineSync()!);
   int? b = int.parse(stdin.readLineSync()!);
-  print(sum(a, b));
+  void sum(double a, int b) => print("sum is ${a + b}");
+  print({sum(a, b)});
+  
   print(diff(a, b));
   print(multiplication(a, b));
   print(divide(a, b));
   return 0;
 }
+
+//SWITH FAT ARROWS
+  int main() {
+  print(
+      " enter numbers between whoem arthemetic operation has to be performes");
+  double? a = double.parse(stdin.readLineSync()!);
+  int? b = int.parse(stdin.readLineSync()!);
+  void sum(double a, int b) => print("sum is ${a + b}");
+  print({sum(a, b)});
+  void diff(double a, int b) => print("difference  is ${a - b}");
+  print({diff(a, b)});
+  void multiplication(double a, int b) => print("multiplication is ${a * b}");
+  print({multiplication(a, b)});
+  void divide(double a, int b) => print("division is ${a / b}");
+  print({divide(a, b)});
+  return 0;
+}
+
