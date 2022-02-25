a937b8691945591583616b3214bfa80790997634
Irya5 <100368720+Irya5@users.noreply.github.com>
2022-02-25 19:44:59 +0530

    Update question4.dart

--- TASK1/question4.dart
+++ TASK1/question4.dart
@@ -1,33 +1,25 @@
 import 'dart:io';
 
-double sum(double a, int b) {
-  print("sum is ");
-  return a + b;
-}
+double sum(double a, int b) => return a + b;
 
-double diff(double a, int b) {
-  print("difference is ");
-  return a - b;
-}
 
-double multiplication(double a, int b) {
-  print("multiplication is ");
-  return a * b;
-}
+double diff(double a, int b) => return a - b;
 
-double divide(double a, int b) {
-  print("divition  is ");
-  return a / b;
-}
+
+double multiplication(double a, int b) => return a * b;
+
+
+double divide(double a, int b) =>return a / b;
+
 
 int main() {
   print(
       " enter numbers between whoem arthemetic operation has to be performes");
   double? a = double.parse(stdin.readLineSync()!);
   int? b = int.parse(stdin.readLineSync()!);
-  print(sum(a, b));
-  print(diff(a, b));
-  print(multiplication(a, b));
-  print(divide(a, b));
+  print("sum is "sum(a, b));
+  print("difference is "diff(a, b));
+  print("multiplication is "multiplication(a, b));
+  print("divition  is "divide(a, b));
   return 0;
 }
