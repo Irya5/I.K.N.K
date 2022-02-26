e751471e78f9e9b4db72c39defb66d88a73588a8
Irya5 <100368720+Irya5@users.noreply.github.com>
2022-02-26 11:01:54 +0530

    Update and rename task to question1.dart

---
+++ TASK2/question1.dart
@@ -0,0 +1,24 @@
+//question 1 answer
+//1st part answer
+//-->int multiply(int a, int b) =>  a * b;
+
+//2nd part answer
+//-->Async functions
+
+//3rd part answer
+//-->list
+
+//4th part answer
+//-->error generated
+
+//5th part answer
+//-->print(‘Your name is ‘{name}’); 
+
+
+//6th part answer
+//-->nums.forEach(print); 
+
+//7th part answer
+//-->Ensures type safety
+//-->Reduce code duplication
+
